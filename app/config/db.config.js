14cc612c3c8f25ba43d4fe664d2142a2f9b9cbc7
Cupara <stevemoore82@outlook.com>
2023-05-11 04:28:48 -0500

    New Files: - app/config/db.config.js - app/controllers/tutorial.controller.js - app/models/index.js - app/models/tutorial.model.js - server.js - src/http-common.js
    Modified Files:
    - package-lock.json
    - package.json
    - src/App.css
    - src/App.js
    - src/index.js

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "MythP0wer@1082",
+    DB: "cms",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/reviews" className="navbar-brand">
+            Reviews
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/reviews"} className="nav-link">
+                Reviews
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<ReviewsList/>} />
+            <Route path="/reviews" element={<ReviewsList/>} />
+            <Route path="/add" element={<AddReview/>} />
+            <Route path="/reviews/:id" element={<Review/>} />
+          </Routes>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
